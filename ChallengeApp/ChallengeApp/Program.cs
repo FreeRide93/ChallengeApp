82fb450d8af532b2064c6b8bcd89b2e0d29918a3
FreeRide93 <freeridepl@gmail.com>
2023-07-28 14:12:32 +0200

    Praca domowa - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool Women = true;
+int age = 29;
+
+if (Women = true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (Women = false && name != "Ewa" && age <= 17)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Brak danych w bazie");
+}
